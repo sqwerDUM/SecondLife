0b655b9176ffc8f47fdf90debe2fbaea52d0b9f6
sqwerDUM <125292907+sqwerDUM@users.noreply.github.com>
2024-06-22 08:16:28 +0600

    Delete src/main/java/com/itacademy/finalprod/project_/config/SecurityConfigurer.java

--- src/main/java/com/itacademy/finalprod/project_/config/SecurityConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.itacademy.finalprod.project_.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
-public interface SecurityConfigurer {
-    void configure(HttpSecurity http) throws Exception;
-}

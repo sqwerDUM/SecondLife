4744dee95b0567cfd01e83846c5a1256592638ca
sqwerDUM <125292907+sqwerDUM@users.noreply.github.com>
2024-06-22 08:19:55 +0600

    Delete src/main/java/com/itacademy/finalprod/project_/repo/BuyerRepository.java

--- src/main/java/com/itacademy/finalprod/project_/repo/BuyerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.itacademy.finalprod.project_.repo;
-
-
-import com.itacademy.finalprod.project_.entity.Buyer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BuyerRepository extends JpaRepository<Buyer,Long> {
-}
